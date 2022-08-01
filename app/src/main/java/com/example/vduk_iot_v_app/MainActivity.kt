4de07693a4fde00982e0fbd52d3a446c1e8ea8af
nb27419 <Michaelmike2512>
2022-08-01 11:34:05 +0100

    feat: update sdk implementation to V2, update idtm to v2.0.25, update documentation

--- app/src/main/java/com/example/vduk_iot_v_app/MainActivity.kt
+++ app/src/main/java/com/example/vduk_iot_v_app/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.vduk_iot_v_app
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.vduk_iot_v_app.databinding.ActivityMainBinding
-import com.vodafone.smartlife.vpartner.MyApplication
-import com.vodafone.smartlife.vpartner.data.repository.LocalPreferencesRepositoryImpl
 import com.vodafone.smartlife.vpartner.domain.usecases.VPartnerLib
 import org.json.JSONObject
 
@@ -13,18 +10,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val sharedPreferences =
-        LocalPreferencesRepositoryImpl.getInstance(MyApplication.application())
-
     private val vPartnerLibIntegration = VPartnerLib(
-        clientId = CLIENT_ID, // defined by partner
-        grantId = GRANT_ID, // defined by partner
+        partnerPassword = PARTNER_PASSWORD,
+        clientId = CLIENT_ID,
+        grantId = GRANT_ID,
         partnerCode = PARTNER_CODE,
         partnerLogo = PARTNER_LOGO,
-        sponsorLogo = ""
+        sponsorLogo = "",
+        locale = "",
+        useDeeplink = false
     )
 
     companion object {
+        const val PARTNER_PASSWORD = ""
         const val GRANT_ID = ""
         const val CLIENT_ID = ""
         const val PARTNER_CODE = "DUMMY_PARTNER"
@@ -43,19 +41,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupButtons() {
-
-        binding.switchPartnerLogo.setOnCheckedChangeListener { _, b ->
-            if (b)
-                vPartnerLibIntegration.storePartnerLogo(PARTNER_LOGO)
-            else
-                vPartnerLibIntegration.storePartnerLogo("")
-        }
-
-        binding.btnLogoutIdtm.setOnClickListener {
-            vPartnerLibIntegration.logout(this)
-            Toast.makeText(this, "Logout successful", Toast.LENGTH_LONG).show()
-        }
-
         binding.btnPartnerAddDevice.setOnClickListener {
             openSdkWithEntryPointAddDevice()
         }
@@ -68,30 +53,14 @@ class MainActivity : AppCompatActivity() {
     private fun openSdkWithEntryPointAddDevice() {
         vPartnerLibIntegration.addDevice(
             this,
-            productId = if (binding.editTextImei.text.toString()
-                .isEmpty()
-            ) PRODUCT_ID else binding.editTextImei.text.toString(),
-            productCode = if (binding.editTextProdcutCode.text.toString()
-                .isEmpty()
-            ) PRODUCT_CODE else binding.editTextProdcutCode.text.toString(),
+            productId = binding.editTextImei.text.toString().ifEmpty { PRODUCT_ID },
+            productCode = binding.editTextProdcutCode.text.toString().ifEmpty { PRODUCT_CODE },
             ::partnerStatusCallback
-        )
-
-        vPartnerLibIntegration.storePartnerCode(
-            if (binding.editTextPartnerCode.text.toString()
-                .isEmpty()
-            ) PARTNER_CODE else binding.editTextPartnerCode.text.toString()
         )
     }
 
     private fun openSdkWithEntryPointManageSubscriptions() {
         vPartnerLibIntegration.manageSubscriptions(this)
-
-        vPartnerLibIntegration.storePartnerCode(
-            if (binding.editTextPartnerCode.text.toString()
-                .isEmpty()
-            ) PARTNER_CODE else binding.editTextPartnerCode.text.toString()
-        )
     }
 
     override fun onBackPressed() {
@@ -100,7 +69,5 @@ class MainActivity : AppCompatActivity() {
 
     private fun partnerStatusCallback(status: String) {
         println("Onboarding status: " + JSONObject(status).getString("status"))
-
-        sharedPreferences.storeOnboardingStatus("{\"status\":\"\"}")
     }
 }
--- app/src/main/java/com/example/vduk_iot_v_app/MyApp.kt
+++ app/src/main/java/com/example/vduk_iot_v_app/MyApp.kt
@@ -1,13 +1,11 @@
 package com.example.vduk_iot_v_app
 
 import com.vodafone.smartlife.vpartner.MyApplication
+import dagger.hilt.android.HiltAndroidApp
 
 /**
  * Extent MyApplication() of library to initialize library and Idtm
  */
-class MyApp : MyApplication() {
 
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
+@HiltAndroidApp
+class MyApp : MyApplication()
